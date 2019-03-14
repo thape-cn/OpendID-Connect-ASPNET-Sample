3bee854bf7a3ad32272f87ec02adfe045ffa4232
Eric Guo <guochunzhong@thape.com.cn>
2019-03-14 13:39:56 +0800

    Change code according to below SO answer.
    https://stackoverflow.com/questions/33661935/owin-middleware-for-openid-connect-code-flow-flow-type-authorizationcode

--- OpenID-Sample/OpenID-Sample/App_Start/Startup.Auth.cs
+++ OpenID-Sample/OpenID-Sample/App_Start/Startup.Auth.cs
@@ -12,7 +12,11 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System.Net.Http;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace OpenID_Sample
 {
@@ -23,6 +27,8 @@ namespace OpenID_Sample
         {
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
+            // Insert a new cookies middleware in the pipeline to store the user
+            // identity after he has been redirected from the identity provider.
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationMode = AuthenticationMode.Active,
@@ -31,37 +37,77 @@ namespace OpenID_Sample
                 ExpireTimeSpan = TimeSpan.FromMinutes(5)
             });
 
+
+            // Insert a new OIDC client middleware in the pipeline.
             app.UseOpenIdConnectAuthentication(
                 new OpenIdConnectAuthenticationOptions
                 {
                     AuthenticationMode = AuthenticationMode.Active,
                     AuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
                     SignInAsAuthenticationType = app.GetDefaultSignInAsAuthenticationType(),
-                    ClientId = "a79acc19c5ddd565007918d7e3a61cd490de86e5ec9179f445802285b4750999",
-                    ClientSecret = "214e85448c1f3c276797ab49e3817804c9555a39b4cc025fa00a135206d8b19d",
+
+                    // Note: setting the Authority allows the OIDC client middleware to automatically
+                    // retrieve the identity provider's configuration and spare you from setting
+                    // the different endpoints URIs or the token validation parameters explicitly.
                     Authority = "https://oauth2id.dev/",
                     RequireHttpsMetadata = true,
                     ResponseType = "code",
-                    Scope = "openid",
-                    RedirectUri = "https://localhost:44352/",
 
+                    // Note: these settings must match the application details inserted in
+                    // the database at the server level (see ApplicationContextInitializer.cs).
+                    ClientId = "a79acc19c5ddd565007918d7e3a61cd490de86e5ec9179f445802285b4750999",
+                    ClientSecret = "214e85448c1f3c276797ab49e3817804c9555a39b4cc025fa00a135206d8b19d",
+                    RedirectUri = "https://localhost:44352/oidc",
+                    PostLogoutRedirectUri = "https://localhost:44352",
+
+                    Scope = "openid",
+
+                    SecurityTokenValidator = new JwtSecurityTokenHandler
+                    {
+                        // Disable the built-in JWT claims mapping feature.
+                        InboundClaimTypeMap = new Dictionary<string, string>()
+                    },
+
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    },
+
+                    // Note: by default, the OIDC client throws an OpenIdConnectProtocolException
+                    // when an error occurred during the authentication/authorization process.
+                    // To prevent a YSOD from being displayed, the response is declared as handled.
                     Notifications = new OpenIdConnectAuthenticationNotifications
                     {
-                        AuthorizationCodeReceived = async (notification) =>
+                        AuthenticationFailed = notification =>
+                        {
+                            if (string.Equals(notification.ProtocolMessage.Error, "access_denied", StringComparison.Ordinal))
+                            {
+                                notification.HandleResponse();
+
+                                notification.Response.Redirect("/");
+                            }
+
+                            return Task.CompletedTask;
+                        },
+
+                        // Retrieve an access token from the remote token endpoint
+                        // using the authorization code received during the current request.
+                        AuthorizationCodeReceived = async notification =>
                         {
                             using (var client = new HttpClient())
                             {
                                 var configuration = await notification.Options.ConfigurationManager.GetConfigurationAsync(notification.Request.CallCancelled);
-                                var request = new HttpRequestMessage(HttpMethod.Get, configuration.TokenEndpoint);
+
+                                var request = new HttpRequestMessage(HttpMethod.Post, configuration.TokenEndpoint);
                                 request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
-                               {
-                                    {OpenIdConnectParameterNames.ClientId, notification.Options.ClientId},
-                                    {OpenIdConnectParameterNames.ClientSecret, notification.Options.ClientSecret},
-                                    {OpenIdConnectParameterNames.Code, notification.ProtocolMessage.Code},
-                                    {OpenIdConnectParameterNames.GrantType, "authorization_code"},
-                                    {OpenIdConnectParameterNames.ResponseType, "token"},
-                                    {OpenIdConnectParameterNames.RedirectUri, notification.Options.RedirectUri}
-                               });
+                                {
+                                    [OpenIdConnectParameterNames.ClientId] = notification.Options.ClientId,
+                                    [OpenIdConnectParameterNames.ClientSecret] = notification.Options.ClientSecret,
+                                    [OpenIdConnectParameterNames.Code] = notification.ProtocolMessage.Code,
+                                    [OpenIdConnectParameterNames.GrantType] = "authorization_code",
+                                    [OpenIdConnectParameterNames.RedirectUri] = notification.Options.RedirectUri
+                                });
 
                                 var response = await client.SendAsync(request, notification.Request.CallCancelled);
                                 response.EnsureSuccessStatusCode();
@@ -70,12 +116,33 @@ namespace OpenID_Sample
 
                                 // Add the access token to the returned ClaimsIdentity to make it easier to retrieve.
                                 notification.AuthenticationTicket.Identity.AddClaim(new Claim(
-                                   type: OpenIdConnectParameterNames.AccessToken,
-                                   value: payload.Value<string>(OpenIdConnectParameterNames.AccessToken)));
+                                    type: OpenIdConnectParameterNames.AccessToken,
+                                    value: payload.Value<string>(OpenIdConnectParameterNames.AccessToken)));
+
+                                // Add the identity token to the returned ClaimsIdentity to make it easier to retrieve.
+                                notification.AuthenticationTicket.Identity.AddClaim(new Claim(
+                                    type: OpenIdConnectParameterNames.IdToken,
+                                    value: payload.Value<string>(OpenIdConnectParameterNames.IdToken)));
                             }
+                        },
+
+                        // Attach the id_token stored in the authentication cookie to the logout request.
+                        RedirectToIdentityProvider = notification =>
+                        {
+                            if (notification.ProtocolMessage.RequestType == OpenIdConnectRequestType.Logout)
+                            {
+                                var token = notification.OwinContext.Authentication.User?.FindFirst(OpenIdConnectParameterNames.IdToken);
+                                if (token != null)
+                                {
+                                    notification.ProtocolMessage.IdTokenHint = token.Value;
+                                }
+                            }
+
+                            return Task.CompletedTask;
                         }
                     }
                 });
+
         }
     }
 }
