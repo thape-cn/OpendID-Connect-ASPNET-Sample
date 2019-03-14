4af5293af638c4fc49452f598c6e01b5c1c5719e
Eric Guo <guochunzhong@thape.com.cn>
2019-03-14 09:53:07 +0800

    Review Authentication code based on:
    https://stackoverflow.com/questions/34058960/owin-openid-connect-authorization-fails-to-authorize-secured-controller-action

--- OpenID-Sample/OpenID-Sample/App_Start/Startup.Auth.cs
+++ OpenID-Sample/OpenID-Sample/App_Start/Startup.Auth.cs
@@ -9,6 +9,10 @@ using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
 using OpenID_Sample.Models;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Security.Claims;
 
 namespace OpenID_Sample
 {
@@ -19,11 +23,20 @@ namespace OpenID_Sample
         {
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
-            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AuthenticationType = "ClientCookie",
+                CookieName = CookieAuthenticationDefaults.CookiePrefix + "ClientCookie",
+                ExpireTimeSpan = TimeSpan.FromMinutes(5)
+            });
 
             app.UseOpenIdConnectAuthentication(
                 new OpenIdConnectAuthenticationOptions
                 {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                    SignInAsAuthenticationType = app.GetDefaultSignInAsAuthenticationType(),
                     ClientId = "a79acc19c5ddd565007918d7e3a61cd490de86e5ec9179f445802285b4750999",
                     ClientSecret = "214e85448c1f3c276797ab49e3817804c9555a39b4cc025fa00a135206d8b19d",
                     Authority = "https://oauth2id.dev/",
@@ -31,6 +44,37 @@ namespace OpenID_Sample
                     ResponseType = "code",
                     Scope = "openid",
                     RedirectUri = "https://localhost:44352/",
+
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthorizationCodeReceived = async (notification) =>
+                        {
+                            using (var client = new HttpClient())
+                            {
+                                var configuration = await notification.Options.ConfigurationManager.GetConfigurationAsync(notification.Request.CallCancelled);
+                                var request = new HttpRequestMessage(HttpMethod.Get, configuration.TokenEndpoint);
+                                request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                               {
+                                    {OpenIdConnectParameterNames.ClientId, notification.Options.ClientId},
+                                    {OpenIdConnectParameterNames.ClientSecret, notification.Options.ClientSecret},
+                                    {OpenIdConnectParameterNames.Code, notification.ProtocolMessage.Code},
+                                    {OpenIdConnectParameterNames.GrantType, "authorization_code"},
+                                    {OpenIdConnectParameterNames.ResponseType, "token"},
+                                    {OpenIdConnectParameterNames.RedirectUri, notification.Options.RedirectUri}
+                               });
+
+                                var response = await client.SendAsync(request, notification.Request.CallCancelled);
+                                response.EnsureSuccessStatusCode();
+
+                                var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                                // Add the access token to the returned ClaimsIdentity to make it easier to retrieve.
+                                notification.AuthenticationTicket.Identity.AddClaim(new Claim(
+                                   type: OpenIdConnectParameterNames.AccessToken,
+                                   value: payload.Value<string>(OpenIdConnectParameterNames.AccessToken)));
+                            }
+                        }
+                    }
                 });
         }
     }
